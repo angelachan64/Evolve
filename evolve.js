22aef8bb4000f0b2472b40bbbcc2ab61e2f5831c
angelachan64 <angelachan64@gmail.com>
2016-03-04 15:12:07 +0000

    setting up the canvas

--- evolve.js
+++ evolve.js
@@ -1,2 +1,10 @@
 var c = document.getElementById("area");
 var ctx = c.getContext("2d");
+
+ctx.beginPath();
+ctx.fillStyle = "#71FF71";
+ctx.fillRect(0, 430, 500, 70);
+ctx.fillStyle = "#FFFFFF";
+ctx.strokeStyle="#000000";
+ctx.strokeRect(500, 0, 200, 500);
+ctx.fillRect(500, 0, 200, 500);
