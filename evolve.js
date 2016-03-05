cecaa0d78d77a2ee17c07c597cfaede0ad9fcaa7
angelachan64 <angelachan64@gmail.com>
2016-03-05 16:00:28 +0000

    health bar updates

--- evolve.js
+++ evolve.js
@@ -4,8 +4,37 @@ var size = 4;
 var tree = new Image();
 var rock = new Image();
 var bush = new Image();
+var health = 200;
+var start = document.getElementById("start");
+var anim;
+
+function update(){
+    
+    /* survivor */
+    ctx.fillStyle = "#8C3449";
+    ctx.arc(250, 430, size, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+    
+    /* bars */
+    ctx.strokeRect(10, 10, 202, 15);
+    ctx.strokeRect(10, 35, 202, 15);
+    ctx.fillRect(11, 11, health, 13);
+
+}
 
 function setup(){
+    /* buttons */
+    tree.src = "/img/tree.png";
+    rock.src = "/img/rock.png";
+    bush.src = "/img/berry.png";
+    bush.onload = function(){
+        ctx.drawImage(tree, 510, 10);
+        ctx.drawImage(rock, 620, 43);
+        ctx.drawImage(bush, 510, 120);
+    };  
+        
+    /* background & sidebar */
     ctx.beginPath();
     ctx.fillStyle = "#71FF71";
     ctx.fillRect(0, 430, 500, 70);
@@ -14,20 +43,6 @@ function setup(){
     ctx.strokeRect(500, 0, 230, 500);
     ctx.fillRect(500, 0, 230, 500);
     ctx.beginPath();
-    
-    ctx.fillStyle = "#8C3449";
-    ctx.arc(250, 430, size, 0, 2*Math.PI);
-    ctx.fill();
-    ctx.closePath();
-
-    tree.src = "/img/tree.png";
-    rock.src = "/img/rock.png";
-    bush.src = "/img/berry.png";
-    bush.onload = function(){
-        ctx.drawImage(tree, 510, 10);
-        ctx.drawImage(rock, 620, 43);
-        ctx.drawImage(bush, 510, 120);
-    }
 }
 
 
@@ -42,6 +57,7 @@ function wiggle(e){
 };
 
 window.onload = setup();
+window.onload = update();
 
 c.addEventListener("mousedown", function(){
   var x = event.x;
@@ -66,3 +82,22 @@ c.addEventListener("mousedown", function(){
       }
   }
 }, false);
+
+start.addEventListener("click", function(){
+    console.log("start");
+    
+    anim = setInterval(function(){
+        ctx.clearRect(0,0,300,70);
+        if(health>0){
+            health = health - 1;
+        }
+        update();
+        console.log(health);
+        
+        if(health==0){
+            clearInterval(anim);
+            console.log("died");
+        }
+    }, 100);
+    
+})
